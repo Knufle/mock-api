34c8beb3ca8457002ea70a0d31b87dbca182d62f
Knufle <marcelbastos12@hotmail.com>
2022-03-07 11:23:02 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# express-simple-qcbfny
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/express-simple-qcbfny)
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3010;
+const cors = require('cors');
+const path = require('path');
+const corsOptions = {
+  origin: 'https://vitejs-vite-7z45ca--3000.local.webcontainer.io',
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+// app.use(express.static('static'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  const test = [{ name: 'John', age: 33 }];
+
+  res.json(test);
+});
+
+app.listen(port);
