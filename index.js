f371cd3b1736cc67eed4709c3d33d474323ea8e6
Knufle <marcelbastos12@hotmail.com>
2022-03-07 11:40:38 -0300

    enabling cors for all

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const corsOptions = {
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 // app.use(express.static('static'));
 app.use(express.json());
 
