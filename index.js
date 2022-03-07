97fda23a8a6f8380a0d6b0dc190d1e842d0b099e
Knufle <marcelbastos12@hotmail.com>
2022-03-07 11:28:10 -0300

    changing port for heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3010;
+const port = process.env.PORT || 3010;
 const cors = require('cors');
 const path = require('path');
 const corsOptions = {
